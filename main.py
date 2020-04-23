2b0f8d432e2310e8762aceb39cfd91ad69bd72a7
max Zulfer <maxzulfer@MacBook-Pro-2.local>
2020-04-23 09:46:39 -0700

    multiple enemies

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame  # always import the library
 import random  # allows random functionality
+import math
 from pygame import mixer  # import if you want music
 
 # initialize the game every time
@@ -14,6 +15,7 @@ background = pygame.image.load('background.png')
 # background music
 mixer.music.load('Max_Invader.wav')
 mixer.music.play(-1)  # Plays the wav in a loop
+pygame.mixer.music.set_volume(0.2)  # value between 0.0 and 1.0
 
 # Title and Icon
 pygame.display.set_caption("Max Invader")
@@ -27,12 +29,19 @@ playerY = 480  # this depends on screen size
 playerX_change = 0  # speed it starts at
 
 # Enemy
-enemyImg = pygame.image.load('ufo.png')
-enemyX = random.randint(0, 800)  # this depends on screen size
-enemyY = random.randint(50, 150)  # make sure to import random for randomized integers here
-enemyX_change = 3  # provides constant movement
-enemyY_change = 0
-enemyY_change = 30  # this will move it down whe it hits border along with conditional statement below
+enemyImg = []
+enemyX = []
+enemyY = []
+enemyX_change = []
+enemyY_change = []
+num_of_enemies = 6
+
+for i in range(num_of_enemies):
+    enemyImg.append(pygame.image.load('ufo.png'))
+    enemyX.append(random.randint(0, 735))  # this depends on screen size
+    enemyY.append(random.randint(50, 150))  # make sure to import random for randomized integers here
+    enemyX_change.append(3)  # provides constant movement
+    enemyY_change.append(30)  # this will move it down whe it hits border along with conditional statement below
 
 # bullet
 # Ready - you cant see the bullet on the screen
@@ -41,22 +50,35 @@ bulletImg = pygame.image.load('bullet.png')
 bulletX = 0  # this depends on screen size
 bulletY = 480  # because spaceship is at 480 pixels
 bulletX_change = 0
-bulletY_change = 30  # speed of bullet
+bulletY_change = 20  # speed of bullet
 bullet_state = "ready"
+
+# score
+
+score_value = 0
 
 
 def player(x, y):  # player function
     screen.blit(playerImg, (x, y))  # this function will draw the icon to the screen
 
 
-def enemy(x, y):  # player function
-    screen.blit(enemyImg, (x, y))  # this function will draw the icon to the screen
+def enemy(x, y, i):  # player function
+    screen.blit(enemyImg[i], (x, y))  # this function will draw the icon to the screen
 
 
 def fire_bullet(x, y):
-    global bullet_state
+    global bullet_state  # make sure to write "global" to use the variable
     bullet_state = "fire"  # changes from "ready"
-    screen.blit(bulletImg, (x + 16, y + 10))  # to make sure it appears in the center of spaceship
+    screen.blit(bulletImg, (x + 16, y + 10))  # to make sure it fires and appears in the center of spaceship
+
+
+# bullet enemy collision
+def isCollision(enemyX, enemyY, bulletX, bulletY):
+    distance = math.sqrt((math.pow(enemyX - bulletX, 2)) + (math.pow(enemyY - bulletY, 2)))
+    if distance < 27:
+        return True
+    else:
+        return False
 
 
 # Game Loop. Anything that is constant will live in this loop
@@ -78,7 +100,9 @@ while running:
             if event.key == pygame.K_RIGHT:
                 playerX_change = 4  # how fast the spaceship will move from left/right
             if event.key == pygame.K_SPACE:
-                fire_bullet(playerX, bulletY)  # calls fire bullet function
+                if bullet_state is "ready":  # checks if bullet is on screen or not
+                    bulletX = playerX  # if not it gets x coordinate of spaceship/ bulletX
+                    fire_bullet(playerX, bulletY)  # calls fire bullet function
 
         if event.type == pygame.KEYUP:  # when a key stroke has been released
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
@@ -92,21 +116,34 @@ while running:
     elif playerX >= 736:  # taking in account for size of spaceship
         playerX = 736
 
-    enemyX += enemyX_change  # key movement functionality
+    for i in range(num_of_enemies):
+        enemyX[i] += enemyX_change[i]  # key movement functionality
+        #  this part will create boundaries for the game
+        if enemyX[i] <= 0:  # when the ship reaches boundaries it deletes it and reprints it at 0
+            enemyX_change[i] = 3
+            enemyY[i] += enemyY_change[i]  # this will move it down when it hits border
+        elif enemyX[i] >= 736:  # taking in account for size of spaceship
+            enemyX_change[i] = -3
+            enemyY[i] += enemyY_change[i]
 
-    #  this part will create boundaries for the game
-    if enemyX <= 0:  # when the ship reaches boundaries it deletes it and reprints it at 0
-        enemyX_change = 3
-        enemyY += enemyY_change  # this will move it down when it hits border
-    elif enemyX >= 736:  # taking in account for size of spaceship
-        enemyX_change = -3
-        enemyY += enemyY_change
+        collision = isCollision(enemyX[i], enemyY[i], bulletX, bulletY)
+        if collision:
+            bulletY = 480
+            bulletX_change = 'ready'
+            score_value += 1
+            enemyX[i] = random.randint(0, 735)  # when hit the enemy wil restart at random pos
+            enemyY[i] = random.randint(50, 150)
+
+        enemy(enemyX[i], enemyY[i], i)
 
     # Bullet movement
+    if bulletY <= 0:  # allowing multiple bullets to be fired
+        bulletY = 480
+        bullet_state = 'ready'
+
     if bullet_state is "fire":
-        fire_bullet(playerX, bulletY)
-        bulletX -= bulletY_change
+        fire_bullet(bulletX, bulletY)
+        bulletY -= bulletY_change
 
     player(playerX, playerY)  # notice how this icon function is being called after the screen background
-    enemy(enemyX, enemyY)
     pygame.display.update()  # this will make sure the screen is always updating
